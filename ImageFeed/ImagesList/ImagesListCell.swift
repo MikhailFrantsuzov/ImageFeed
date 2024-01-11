7bbcd48b76f2182fb870bc86cfd436a5bb1a56a9
MikhailFrantsuzov <m.francuzov@gmail.com>
2024-01-11 12:03:20 +0100

    Project

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -9,5 +9,8 @@ import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
 }
