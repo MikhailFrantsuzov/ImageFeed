8b720b4139cb89ae0080f91943370e45e5158b05
MikhailFrantsuzov <m.francuzov@gmail.com>
2024-01-11 11:53:04 +0100

    Спринт №8

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Mikhail Frantsuzov on 11.01.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+}
